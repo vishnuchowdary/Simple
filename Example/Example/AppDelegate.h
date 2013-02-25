a315681dcd6e0a0ad61463d1f340b3ded6b59e54
vishnu <vishnu@hexpress.net>
2013-02-25 15:36:20 +0530

    commit

---
+++ Example/Example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by vishnu on 25/02/13.
+//  Copyright (c) 2013 Hexpress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
